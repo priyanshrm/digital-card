ab8acc70a60aaea066f656cdb58f3cc13b0ea7b3
priyanshrm <priyanshmahendra@gmail.com>
2023-02-20 18:09:24 +0530

    Add links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,12 +4,28 @@ import twitterIcon from '../../assets/Twitter Icon.png';
 import igIcon from '../../assets/Instagram Icon.png';
 
 export default function Footer(){
+    const twitterUrl = 'https://twitter.com/priyanshrajcode'
+    const githubUrl = 'https://github.com/priyanshrm'
+    const instagramUrl = 'https://instagram.com/priyanshrajmahendra'
+    const facebookUrl = 'https://www.facebook.com/priyanshrajmahendra'
+    function twitterClick(params) {
+        window.open(twitterUrl, '_blank')
+    }
+    function githubClick(params) {
+        window.open(githubUrl, '_blank')
+    }
+    function igClick(params) {
+        window.open(instagramUrl, '_blank')
+    }
+    function facebookClick(params) {
+        window.open(facebookUrl, '_blank')
+    }
     return (
         <footer>
-            <img src={twitterIcon}></img>
-            <img src={fbIcon}></img>
-            <img src={igIcon}></img>
-            <img src={githubIcon}></img>
+            <img src={twitterIcon} onClick={twitterClick} />
+            <img src={fbIcon} onClick={facebookClick} />
+            <img src={igIcon} onClick={igClick} />
+            <img src={githubIcon} onClick={githubClick} />
         </footer>
     )
 }
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,26 +2,40 @@ import mailImg from '../../assets/Mail.png'
 import linkedinImg from '../../assets/linkedin.png'
 
 export default function Main() {
+    const linkedinUrl = 'https://www.linkedin.com/in/priyanshrajmahendra/'
+    const emailId = 'priyanshmahendra@gmail.com'
+
+    function linkedinClick(params) {
+        window.open(linkedinUrl, '_blank')
+    }
+
+    function emailClick(params) {
+        window.location.href = `mailto:${emailId}`;
+    }
+
+
     return (
         <main>
             <h3 className="name">Priyansh Raj Mahendra</h3>
-            <h5 className="job">Frontend Developer</h5>
-            <h6 className="website">priyansh.website</h6>
+            <h5 className="job">Software Engineer</h5>
+            <h6 className="website">(Python, Reactjs, Deep Learning)</h6>
             <div className="buttons">
-                <button className='mail-btn'>
+                <button className='mail-btn' onClick={emailClick}>
                     <img src={mailImg} alt="mail-png"></img>
                     <p>Email</p>
                 </button>
-                <button className='linkedin-btn'>
+                <button className='linkedin-btn' onClick={linkedinClick}>
                     <img src={linkedinImg} alt="linkin-png"></img>
                     <p>LinkedIn</p>
                 </button>
             </div>
             <div className='main-txt'>
                 <h4>About</h4>
-                <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+                <p>I am a coder, and I believe that the power to build lies within the lines of code 
+                    that I create. With every keystroke, I am able to bring my ideas to life, 
+                    to solve complex problems and to make the impossible, possible. </p>
                 <h4>Interests</h4>
-                <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+                <p>Football fan. Music scholar. Internet fanatic. Pop culture ninja. Coffee fanatic. Finance enthusiastic. </p>
             </div>
         </main>
     )
